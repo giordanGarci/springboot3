41d2a7cd9ee2490c24b45568b24fe30e5b289cb8
Giordan Garcia <giordangarcia12@gmail.com>
2023-08-27 15:59:06 -0300

    Category

--- src/main/java/com/coursespring/course/config/TestConfig.java
+++ src/main/java/com/coursespring/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.coursespring.course.entities.Category;
 import com.coursespring.course.entities.Order;
 import com.coursespring.course.entities.User;
 import com.coursespring.course.entities.enums.OrderStatus;
+import com.coursespring.course.repositories.CategoryRepository;
 import com.coursespring.course.repositories.OrderRepository;
 import com.coursespring.course.repositories.UserRepository;
 
@@ -18,6 +20,9 @@ import com.coursespring.course.repositories.UserRepository;
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 
+	@Autowired
+	CategoryRepository categoryRepository;
+	
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -33,9 +38,15 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.DELIVERED);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.PAID);
 		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 	
 }
